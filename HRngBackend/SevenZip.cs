086c20c809f57f40caba1ebae5e2891195df38f4
Nguyen Thanh Vinh <ngtv0404@gmail.com>
2022-01-01 13:23:35 +0700

    fix: Make sure that 7za is executable on Linux/Unix (2)

--- HRngBackend/SevenZip.cs
+++ HRngBackend/SevenZip.cs
@@ -75,15 +75,18 @@ namespace HRngBackend
             }
 
             /* Make sure that 7za is executable (Linux/Unix only) */
-            Process chmod = new Process();
-            chmod.StartInfo.FileName = "chmod";
-            chmod.StartInfo.Arguments = $"+x {BinaryPath}";
-            chmod.StartInfo.UseShellExecute = false;
-            chmod.StartInfo.RedirectStandardOutput = true;
-            chmod.StartInfo.CreateNoWindow = true;
-            chmod.Start();
-            chmod.StandardOutput.ReadToEnd();
-            chmod.WaitForExit();
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Process chmod = new Process();
+                chmod.StartInfo.FileName = "chmod";
+                chmod.StartInfo.Arguments = $"+x {BinaryPath}";
+                chmod.StartInfo.UseShellExecute = false;
+                chmod.StartInfo.RedirectStandardOutput = true;
+                chmod.StartInfo.CreateNoWindow = true;
+                chmod.Start();
+                chmod.StandardOutput.ReadToEnd();
+                chmod.WaitForExit();
+            }
 
             return true;
         }
